29df07d08211e635dbb2599599e23b67f1574da6
Greg Allen <me@jga.me>
2013-08-02 10:34:21 -0700

    updated package versions

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -139,6 +139,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-plato');
   grunt.registerTask('scripts', ['jshint', 'bower', 'concat', 'uglify', 'clean:bower', 'mocha', 'bytesize']);
   grunt.registerTask('default', ['scripts']);
-  grunt.registerTask('dev', ['connect:server', 'watch']);
+  grunt.registerTask('dev', ['default', 'connect:server', 'watch']);
   grunt.registerTask('reports', ['plato', 'connect:plato']);
 };
