144330bc5b920be60c19cbe0d61a8f945142ff98
Greg Allen <me@jga.me>
2013-09-05 11:22:14 -0700

    updates

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -128,17 +128,7 @@ module.exports = function(grunt) {
       }
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-concat-bower');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-bytesize');
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.loadNpmTasks('grunt-plato');
+  require('load-grunt-tasks')(grunt);
   grunt.registerTask('scripts', ['jshint', 'bower', 'concat', 'uglify', 'clean:bower', 'mocha', 'bytesize']);
   grunt.registerTask('default', ['scripts']);
   grunt.registerTask('dev', ['connect:server', 'watch']);
